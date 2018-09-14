48e81442f7c7fa07db5956ae254649f4a402ccd8
hukekuan <hukekuan@163.com>
2018-09-14 21:02:15 +0800

    初始提交

--- apps/spatial/view.py
+++ apps/spatial/view.py
@@ -1,11 +1,21 @@
 #-*- coding:utf-8 -*-
 #!/usr/bin/env python
-
-from flask_restful import Resource
+from flask import url_for, redirect, request
+from flask_restful import Resource, reqparse
 from apps.spatial import spatial
 
 
-@spatial.resource('/')
-class HelloWorld(Resource):
+@spatial.resource('/index', endpoint='index')
+class Index(Resource):
     def get(self):
-        return {'hello': 'API'}
+        parser = reqparse.RequestParser()
+        parser.add_argument('user_name', type=int, help='名称')
+        args = parser.parse_args()
+        user_name = args['user_name']
+        return {'hello': user_name}
+
+# 设置端点
+# @spatial.resource('/endpoint')
+# class TestWorld(Resource):
+#     def get(self):
+#         return redirect(url_for('spatial.index', user_name='cde'))
